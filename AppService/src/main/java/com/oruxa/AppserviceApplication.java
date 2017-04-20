419b2748ae27ddd54ce944a28e92d66192beed58
Navid Esfahani <navid.esfahani@gmail.com>
2017-04-19 20:22:03 -0400

    Init project structure

---
+++ AppService/src/main/java/com/oruxa/AppserviceApplication.java
@@ -0,0 +1,12 @@
+package com.oruxa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppserviceApplication.class, args);
+	}
+}
